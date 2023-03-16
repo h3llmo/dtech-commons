175cf8280fe0ec0d3ddda295cf038b02b755127a
h3llmo <h3llmo@gmail.com>
2023-03-16 15:44:19 +0100

    Bootstrap common-util library

---
+++ dtech-common-utils/src/main/java/com/damosoft/dtech/common/utils/DTMapUtils.java
@@ -0,0 +1,10 @@
+package com.damosoft.dtech.common.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class DTMapUtils {
+    public static Map<String, String> newStringMap() {
+        return new HashMap<>();
+    }
+}
